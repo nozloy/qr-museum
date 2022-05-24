74e07f775bff4e3844785f51acd6b8ec3d78f262
nozloy <31240637+nozloy@users.noreply.github.com>
2022-05-25 02:51:21 +0300

    audio test

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
+import audiofile from '../assets/audio/SoundHelix-Song-1.mp3'
 
 function MediaPlayer() {
 	return (
@@ -9,7 +12,7 @@ function MediaPlayer() {
 				animate={{ opacity: '1', x: 0 }}
 				exit={{ opacity: '1', x: 500, transition: { duration: '0.5' } }}
 			>
-				MediaPlayer
+				<AudioPlayer src={audiofile} onPlay={(e) => console.log('onPlay')} />
 			</motion.div>
 		</div>
 	)
