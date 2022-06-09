ea018aec186e4158a8f47a7b2f8c4d54048fa379
nozloy <31240637+nozloy@users.noreply.github.com>
2022-06-09 09:36:21 +0300

    adding map

--- src/components/Buildings.js
+++ src/components/Buildings.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useBuildingStore } from '../data/stores/BuildingStore.tsx'
+import Maps from '../components/Maps'
 
 const Buildings = () => {
 	const buildings = useBuildingStore((state) => state.buildings)
 	return (
-		<div className='flex flex-col justify-center m-4 rounded-xl neo p-5'>
-			{buildings.map((building) => {
-				return (
-					<Link key={building.captionName} to={building.routerLink}>
-						<button
-							className='text-slate-700 w-5/6 border-2 m-2 p-2 mx-auto rounded-xl neo disabled:bg-slate-400 disabled:text-gray-200'
-							disabled={!building.isVisited}
-						>
-							{building.captionName}
-						</button>
-					</Link>
-				)
-			})}
+		<div>
+			<div className='flex flex-col justify-center m-4 rounded-xl neo p-5'>
+				{buildings.map((building) => {
+					return (
+						<Link key={building.captionName} to={building.routerLink}>
+							<button
+								className='text-slate-700 w-5/6 border-2 m-2 p-2 mx-auto rounded-xl neo disabled:bg-slate-400 disabled:text-gray-200'
+								disabled={!building.isVisited}
+							>
+								{building.captionName}
+							</button>
+						</Link>
+					)
+				})}
+			</div>
+			<div className='flex justify-center m-4 rounded-xl neo p-5'>
+				<Maps />
+			</div>
 		</div>
 	)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,25 +7,27 @@ const Home = () => {
 	const [resetVisited] = useBuildingStore((state) => [state.resetVisited])
 	const [setNotVisited] = LocalStore((state) => [state.setNotVisited])
 	return (
-		<div className='flex flex-col justify-center m-4 p-10 rounded-xl neo'>
-			<h1 className='text-2xl font-bold'>Сервисное меню</h1>
-			<button
-				onClick={() => setNotVisited(true)}
-				className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'
-			>
-				Сбросить посещение сайта
-			</button>
-			<button
-				onClick={() => resetVisited()}
-				className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'
-			>
-				Сбросить посещение зданий
-			</button>
-			<Link to={{ pathname: '/buildings' }}>
-				<button className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'>
-					Список зданий
+		<div>
+			<div className='flex flex-col justify-center m-4 p-10 rounded-xl neo'>
+				<h1 className='text-2xl font-bold'>Сервисное меню</h1>
+				<button
+					onClick={() => setNotVisited(true)}
+					className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'
+				>
+					Сбросить посещение сайта
 				</button>
-			</Link>
+				<button
+					onClick={() => resetVisited()}
+					className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'
+				>
+					Сбросить посещение зданий
+				</button>
+				<Link to={{ pathname: '/buildings' }}>
+					<button className='w-full rounded-xl neo border-2 mx-auto mt-10 p-2'>
+						Список зданий
+					</button>
+				</Link>
+			</div>
 		</div>
 	)
 }
